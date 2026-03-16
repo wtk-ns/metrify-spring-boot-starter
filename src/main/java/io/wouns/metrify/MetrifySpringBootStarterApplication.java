b8685c5b39d2e2c2358cf681167ee261b59e5c8a
wouns <wouns@wns.io>
2026-03-16 13:12:39 +0100

    initial commit

---
+++ src/main/java/io/wouns/metrify/MetrifySpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package io.wouns.metrify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetrifySpringBootStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MetrifySpringBootStarterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/wouns/metrify/MetrifySpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package io.wouns.metrify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetrifySpringBootStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
